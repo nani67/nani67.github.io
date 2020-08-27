938cd95bcf78da59815f593d8d9b6b03a1b8f2f6
P Nani <pnani18dec@gmail.com>
2020-08-27 21:31:51 +0530

    Light and Dark mode

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {
 "index.html": "c0cdfee83a941a47dde3184d6da74a8d",
 "/": "c0cdfee83a941a47dde3184d6da74a8d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "da0300e5653028e412f3454a094ff89d"
+"main.dart.js": "1ea27f2670a776a1be1d75e68b7c7df0"
 };
 
 // The application shell files that are downloaded before a service worker can
